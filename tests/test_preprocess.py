16dfed6c2fe1ef2e4b18e4648c293ab7fb32be70
Andre Lukas Schorlemmer <andre.schorlemmer@gmail.com>
2017-05-24 10:05:37 -0700

    Added test for tokenizer

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,10 +1,22 @@
-import pytest
+import capstone_project.preprocessor as pre
+
 
 def test_tokenize():
+    test_strings = [(u"This is a test.",
+                     [u"test"]),
+                    (u"Are there any positive benefits of laziness and procrastination?",
+                     [u"positive", u"benefit", u"laziness", u"procrastination"]),
+                    (u"A question for testing/trying.",
+                     [u"question", u"testing/try"])]
+
+    for test_string, result in test_strings:
+        output = pre.tokenize(test_string)
+        assert output == result
+
+
+def test_tfidftransformer():
     pass
 
-def test_TfidfTransformer():
-    pass
 
-def test_FeatureTransformer():
-    pas
+def test_featuretransformer():
+    pass
