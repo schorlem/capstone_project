8de70c6f4aecd728a4a4b7562e22208d4ae9506c
Andre Lukas Schorlemmer <andre.schorlemmer@gmail.com>
2017-06-07 18:46:42 -0700

    Created new features and added features scaling(still broken)

--- capstone_project/preprocessor.py
+++ capstone_project/preprocessor.py
@@ -7,11 +7,13 @@ from scipy.sparse import hstack
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.base import BaseEstimator, TransformerMixin
+from scipy.spatial.distance import cosine, cityblock, jaccard, canberra, euclidean, minkowski, braycurtis
+from scipy.stats import skew, kurtosis
 from capstone_project.models import benchmark_model
 
 
 # Create english spacy instance. Very slow to load and should thus only be done once
-nlp = spacy.load('en')
+nlp = spacy.load("en")
 # List of stopwords that will be removed during tokenization
 STOPLIST = stopwords.words("english")
 # List of punctuation symbols that will be removed during tokenization
@@ -70,8 +72,8 @@ class TfidfTransformer(BaseEstimator, TransformerMixin):
                                      preprocessor=None, tokenizer=tokenize)
 
     def transform(self, df, **transform_params):
-        tfidfs_q1 = self.tfidf.transform(df['question1'])
-        tfidfs_q2 = self.tfidf.transform(df['question2'])
+        tfidfs_q1 = self.tfidf.transform(df["question1"])
+        tfidfs_q2 = self.tfidf.transform(df["question2"])
         # Stack horizontally since the words in q1 and q2 should be arranged in separate columns
         tfidf_q1_q2 = hstack([tfidfs_q1, tfidfs_q2])
 
@@ -79,7 +81,7 @@ class TfidfTransformer(BaseEstimator, TransformerMixin):
 
     def fit(self, df, y=None, **fit_params):
         # Fit tfidfs on the whole corpus
-        self.tfidf.fit(pd.concat([df['question1'], df['question2']]))
+        self.tfidf.fit(pd.concat([df["question1"], df["question2"]]))
 
         return self
 
@@ -87,17 +89,31 @@ class TfidfTransformer(BaseEstimator, TransformerMixin):
 class FeatureTransformer(BaseEstimator, TransformerMixin):
 
     def transform(self, df):
-        # https://www.linkedin.com/pulse/duplicate-quora-question-abhishek-thakur
-        # and https://github.com/abhishekkrthakur/is_that_a_duplicate_quora_question
+        """Transform tokenized words into features. My guideline for the feature engineering was
+         the following article https://www.linkedin.com/pulse/duplicate-quora-question-abhishek-thakur.
+        """
         new_data = pd.DataFrame()
         new_data["q1_length"] = df["question1"].apply(lambda question: len(str(question)))
         new_data["q2_length"] = df["question2"].apply(lambda question: len(str(question)))
-        new_data['diff_length'] = new_data["q1_length"] - new_data["q2_length"]
+        new_data["diff_length"] = new_data["q1_length"] - new_data["q2_length"]
         new_data["q1_n_words"] = df["q1_tokens"].apply(lambda words: len(words))
         new_data["q2_n_words"] = df["q2_tokens"].apply(lambda words: len(words))
         new_data["q1_len_word_ratio"] = new_data["q1_length"]/new_data["q1_n_words"]
         new_data["q2_len_word_ratio"] = new_data["q2_length"]/new_data["q2_n_words"]
         new_data["word_share"] = df.apply(benchmark_model.word_match_share, axis=1)
+        new_data["word2vec_cosine_distance"] = df.apply(lambda x: cosine(x["q1_vecsum"], x["q2_vecsum"]), axis=1)
+        new_data["word2vec_cityblock_distance"] = df.apply(lambda x: cityblock(x["q1_vecsum"],
+                                                                              x["q2_vecsum"]), axis=1)
+        new_data["word2vec_jaccard_distance"] = df.apply(lambda x: jaccard(x["q1_vecsum"], x["q2_vecsum"]), axis=1)
+        new_data["word2vec_canberra_distance"] = df.apply(lambda x: canberra(x["q1_vecsum"], x["q2_vecsum"]), axis=1)
+        new_data["word2vec_minkowski_distance"] = df.apply(lambda x: minkowski(x["q1_vecsum"], x["q2_vecsum"], 3), axis=1)
+        new_data["word2vec_euclidean_distance"] = df.apply(lambda x: euclidean(x["q1_vecsum"], x["q2_vecsum"]), axis=1)
+        new_data["word2vec_braycurtis_distance"] = df.apply(lambda x: braycurtis(x["q1_vecsum"], x["q2_vecsum"]), axis=1)
+        new_data["word2vec_skew_q1"] = df["q1_vecsum"].apply(lambda vector: skew(vector))
+        new_data["word2vec_skew_q2"] = df["q2_vecsum"].apply(lambda vector: skew(vector))
+        new_data["word2vec_kurtosis_q1"] = df["q1_vecsum"].apply(lambda vector: kurtosis(vector))
+        new_data["word2vec_kurtosis_q2"] = df["q2_vecsum"].apply(lambda vector: kurtosis(vector))
+
         return new_data
 
     def fit(self, df, y=None, **fit_params):
